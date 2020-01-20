0d7c337bab9c7cc74d0c85746aef947162ed89d0
coltborg <coltborg@gmail.com>
2020-01-19 19:02:23 -0600

    Add more vue rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,27 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase', {
+      registeredComponentsOnly: true,
+      ignores: [],
+    }],
+    'vue/html-closing-bracket-newline': ['error', {
+      singleline: 'never',
+      multiline: 'always',
+    }],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 1,
+      multiline: {
+        max: 1,
+        allowFirstLine: false,
+      },
+    }],
   },
   parserOptions: {
     parser: 'babel-eslint',
